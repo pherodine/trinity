dac99b9a2742a7473b509b0cfabd22db953631ab
Aaron Smyth <aaron@fixrdigital.co.uk>
2018-05-13 03:46:58 +0100

    Updated 404 will need revisiting

--- 404.php
+++ 404.php
@@ -9,25 +9,8 @@
         'items_wrap' => '<ul id="fd-header-nav-menu">%3$s</ul>'
     );
 ?>
-        <section id="section-404">
-            <div class="container">
-                <div class="row">
-                    <div class="introduction block"></div>
-                    <div class="recent col-xs-12">
-                        <div class="ratio-16-9">
-                            <div class="ratio--child cover" style="background-image: url('<?php bloginfo('template_url')?>/assets/images/404.jpg')"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="section-title">
-                        <h2 class="block">Page Not Found</h2>
-                    </div>
-                    <p>Unfortunately you have used a URL that does not exists. Maybe one of the following pages will be of some use to you</p>
-                </div>
-                <div class="row">
-                    <?php wp_nav_menu($_settings); ?>
-                </div>
-            </div>
-        </section>
+<section class="shell__body">
+    <p>Unfortunately you have used a URL that does not exists. Maybe one of the following pages will be of some use to you</p>
+    <?php wp_nav_menu($_settings); ?>
+</section>
 <?php get_template_part('modules/dry-fixr-footer'); ?>
