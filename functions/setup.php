39cbfbce48e46bc945ed022e1f07776824ed71f9
Aaron Smyth <aaron@fixrdigital.co.uk>
2018-05-13 10:30:18 +0100

    Enabled Tools Menu

--- functions/setup.php
+++ functions/setup.php
@@ -140,7 +140,7 @@ function remove_menus(){
   //remove_menu_page( 'themes.php' );                 //Appearance
   //remove_menu_page( 'plugins.php' );                //Plugins
   //remove_menu_page( 'users.php' );                  //Users
-  remove_menu_page( 'tools.php' );                  //Tools
+  //remove_menu_page( 'tools.php' );                  //Tools
   //remove_menu_page( 'options-general.php' );        //Settings
   
 }
