5af045793f8f5f9e50c5ac02d75a01d49658e334
Aaron Smyth <aaron@fixrdigital.co.uk>
2018-05-14 23:37:09 +0100

    Updated theme header and footer

--- modules/header.php
+++ modules/header.php
@@ -1,23 +1,63 @@
-<div class="shell shell--max">
-    <header class="shell__header">
-        <a class="shell__header-brand" href="#">FIXR Digital</a>
-        <a class="shell__header-toggle" href="#">Menu</a>
-        <nav class="shell__header-nav shell__header-nav--hide">
-            <ul class="shell__header-nav__list">
-                <li class="shell__header-nav__list-item">
-                    <a class="shell__header-nav-list-item-link" href="#">Home</a>
-                </li>
-                <li class="shell__nav-item">
-                    <a class="shell__nav-link" href="#">Welcome</a>
-                </li>
-                <li class="shell__nav-item">
-                    <a class="shell__nav-link" href="#">Services</a>
-                </li>
-                <li class="shell__nav-item">
-                    <a class="shell__nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+<?php
+    /**
+     * The main template file
+     *
+     * This is the template that displays all of the <head> section and everything up until <div id="content">
+     *
+     * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+     *
+     * @package fixr
+     */
 
-    <main class="shell__body">
+    // check advanced custom fields is installed
+	$has_acf = (class_exists('acf')) ? true : false;
+
+	// Use custom page if it has been set
+	$checked = get_field('custom_page_title');
+	$page_title = "";
+	
+	if($checked && !empty($checked)) {
+		$page_title = get_field('custom_page_title');
+	} else {
+		$page_title = wp_title('-', false, 'right');
+	}    
+?>
+<?php define('WPLANG', 'en-GB'); ?>
+<html <?php language_attributes(); ?>>
+<head>
+    <title><?php echo $page_title; ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="https://fixrdigital.co.uk">
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php wp_head(); ?>
+</head>
+    
+<body <?php body_class(); ?>>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC9X6BX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <div class="shell shell--max">
+        <header class="shell__header">
+            <a class="shell__header-brand" href="#">FIXR Digital</a>
+            <a class="shell__header-toggle" href="#">Menu</a>
+            <nav class="shell__header-nav shell__header-nav--hide">
+                <ul class="shell__header-nav__list">
+                    <li class="shell__header-nav__list-item">
+                        <a class="shell__header-nav-list-item-link" href="#">Home</a>
+                    </li>
+                    <li class="shell__nav-item">
+                        <a class="shell__nav-link" href="#">Welcome</a>
+                    </li>
+                    <li class="shell__nav-item">
+                        <a class="shell__nav-link" href="#">Services</a>
+                    </li>
+                    <li class="shell__nav-item">
+                        <a class="shell__nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <main class="shell__body">
--- src/modules/footer.php
+++ src/modules/footer.php
@@ -1,67 +1,93 @@
-    </main>
+            </main>
 
-    <footer class="shell__footer">
+            <footer class="shell__footer">
 
-        <a href="#" class="shell__footer-brand">FIXR Digital</a>
+                <a href="#" class="shell__footer-brand">FIXR Digital</a>
 
-        <p class="shell__footer-tagline">Breaking the Rules</p>
+                <p class="shell__footer-tagline">Breaking the Rules</p>
 
-        <div class="shell__footer-ethos">
-            <h1 class="shell__footer-ethos__heading">Building Tomorrow's Websites Today</h1>
-            <a class="shell__footer-ethos__email" href="#">hi@fixrdigital.co.uk</a>
-        </div>
+                <div class="shell__footer-ethos">
+                    <h1 class="shell__footer-ethos__heading">Building Tomorrow's Websites Today</h1>
+                    <a class="shell__footer-ethos__email" href="#">hi@fixrdigital.co.uk</a>
+                </div>
 
-        <address class="shell__footer-locale">
-            <p>
-                <b>Locale</b>
-                <br> 11 Thackeray
-                <br>Horfield
-                <br>Bristol
-                <br>BS7 0NX</p>
-        </address>
+                <address class="shell__footer-locale">
+                    <p>
+                        <b>Locale</b>
+                        <br> 11 Thackeray
+                        <br>Horfield
+                        <br>Bristol
+                        <br>BS7 0NX</p>
+                </address>
 
-        <div class="shell__footer-contact">
-            <ul class="shell__footer-contact__list">
-                <li>
-                    <b>Get Started</b>
-                </li>
-                <li>
-                </li>
-                <li class="shell__footer-contact__list-item">
-                    <a class="shell__footer-contact__link" href="#">info@fixrdigital.co.uk</a>
-                </li>
-                <li class="shell__footer-contact__list-item">
-                    <a class="shell__footer-contact__link" href="#">0117-951-5060</a>
-                </li>
-            </ul>
-        </div>
+                <div class="shell__footer-contact">
+                    <ul class="shell__footer-contact__list">
+                        <li>
+                            <b>Get Started</b>
+                        </li>
+                        <li>
+                        </li>
+                        <li class="shell__footer-contact__list-item">
+                            <a class="shell__footer-contact__link" href="#">info@fixrdigital.co.uk</a>
+                        </li>
+                        <li class="shell__footer-contact__list-item">
+                            <a class="shell__footer-contact__link" href="#">0117-951-5060</a>
+                        </li>
+                    </ul>
+                </div>
 
-        <div class="shell__footer-recent">
-            <ul class="shell__footer-recent__list">
-                <li>
-                    <b>Recent</b>
-                </li>
-                <li class="shell__footer-recent__list-item">
-                    <a class="shell__footer-recent__link" href="#">Minster Partnership</a>
-                </li>
-                <li class="shell__footer-recent__list-item">
-                    <a class="shell__footer-recent__link" href="#">North Star Wealth</a>
-                </li>
-            </ul>
-        </div>
+                <div class="shell__footer-recent">
+                    <ul class="shell__footer-recent__list">
+                        <li>
+                            <b>Recent</b>
+                        </li>
+                        <li class="shell__footer-recent__list-item">
+                            <a class="shell__footer-recent__link" href="#">Minster Partnership</a>
+                        </li>
+                        <li class="shell__footer-recent__list-item">
+                            <a class="shell__footer-recent__link" href="#">North Star Wealth</a>
+                        </li>
+                    </ul>
+                </div>
 
-        <div class="shell__footer-social">
-            <ul class="shell__footer-social__list">
-                <li>
-                    <b>Social</b>
-                </li>
-                <li class="shell__footer-social__list-item">
-                    <a class="shell__footer-social__link" href="#">Facebook</a>
-                </li>
-                <li class="shell__footer-social__list-item">
-                    <a class="shell__footer-social__link" href="#">Twitter</a>
-                </li>
-            </ul>
-        </div>
-    </footer>
-</div><!-- /.shell -->
+                <div class="shell__footer-social">
+                    <ul class="shell__footer-social__list">
+                        <li>
+                            <b>Social</b>
+                        </li>
+                        <li class="shell__footer-social__list-item">
+                            <a class="shell__footer-social__link" href="#">Facebook</a>
+                        </li>
+                        <li class="shell__footer-social__list-item">
+                            <a class="shell__footer-social__link" href="#">Twitter</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </div><!-- /.shell -->
+        <?php wp_footer(); ?>
+        <!--
+		<noscript id="deferred-styles">
+			<link rel="stylesheet" type="text/css" href="#" media="screen" />
+		</noscript>
+		<script>
+			var loadDeferredStyles = function() {
+				var addStylesNode = document.getElementById("deferred-styles");
+				var replacement = document.createElement("div");
+		
+				replacement.innerHTML = addStylesNode.textContent;
+				document.body.appendChild(replacement)
+				addStylesNode.parentElement.removeChild(addStylesNode);
+			};
+			var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+				window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+			if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+			else window.addEventListener('load', loadDeferredStyles);
+		</script>
+        -->
+		<!--
+		<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion_async.js" charset="utf-8"></script>
+		<noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/960583903/?label=ahZuCNuKrlkQ37GFygM&amp;guid=ON&amp;script=0"/></div></noscript>
+		-->
+	</body>
+</html>
